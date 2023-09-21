0d65a45077f1bdb393834a12d62feda71ab41273
Bluesadsilk <bluesadsilk@proton.me>
2023-09-21 18:28:15 +0200

    commit vacio

--- Proyecto_propio/proyecto_propio.java
+++ Proyecto_propio/proyecto_propio.java
@@ -3,6 +3,19 @@ package Proyecto_propio;
 public class proyecto_propio {
 
     public static void main(String[] args) {
+        String Ej7 = "La forma de introducir unas \" dentro de la frase es utilizando los caracteres especiales";
+        int numeroOctal = 1007;
+        int numeroHexadecimal = 207;
+        double numeroExponencial = 16.55e0;
+        long numeroLargo = 2345L;
+        float numeroFloat = 0.5F;
+
+        System.out.println(numeroOctal);
+        System.out.println(Ej7);
+        System.out.println(numeroHexadecimal);
+        System.out.println(numeroExponencial);
+        System.out.println(numeroLargo);
+        System.out.println(numeroFloat);
 
     }
 }
