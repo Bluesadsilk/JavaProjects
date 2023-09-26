9c716fa00bff0f5732680794928f498372676081
Bluesadsilk <bluesadsilk@proton.me>
2023-09-26 15:21:19 +0200

    Ejercicio 3

--- Proyecto_3/proyecto3.java
+++ Proyecto_3/proyecto3.java
@@ -9,19 +9,26 @@ public class proyecto3 {
 
     public static void main(String[] args) {
 
-        System.out.println("Introduce tu nombre completo");
+        System.out.println("Introduce tu nombre");
 
         Scanner tecladoNombre = new Scanner(System.in);
         String nombreCompleto = tecladoNombre.nextLine();
+
+        System.out.println("Introduce tu apellido");
+
+        Scanner tecladoApellido = new Scanner(System.in);
+        String apellidoCompleto = tecladoApellido.nextLine();
 
         System.out.println("Introduce tu fecha de nacimiento");
 
         Scanner tecladoFecha = new Scanner(System.in);
         String fechaNacimiento = tecladoFecha.nextLine();
+
         tecladoFecha.close();
         tecladoNombre.close();
+        tecladoApellido.close();
 
-        System.out.println("Bienvenido " + nombreCompleto + " nacido en " + fechaNacimiento);
+        System.out.println("Bienvenido " + nombreCompleto + " " + apellidoCompleto + " nacido en " + fechaNacimiento);
 
     }
 }
