9c73bf91844ff31cd2b72065d259db500a2cef1f
Bluesadsilk <bluesadsilk@proton.me>
2023-09-26 19:04:34 +0200

    Calc

--- EjerciciosDatos4/Calculadora_Segundos/calculadoraSegundos.java
+++ EjerciciosDatos4/Calculadora_Segundos/calculadoraSegundos.java
@@ -1,11 +1,18 @@
 package EjerciciosDatos4.Calculadora_Segundos;
 
+import java.util.Scanner;
+
 public class calculadoraSegundos {
 
     public static void main(String[] args) {
 
-        System.out.println("Introduzca un numero  indeterminado de segundos");
-        System.out.println("El resultado de la conversión es");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca un numero indeterminado de segundos");
+        long totalSegundos = teclado.nextLong();
+        teclado.close();
+        long conversor = 1;
+        System.out.println("El total en dias, horas y minutos es");
+
     }
 
 }
