2a21c631cb6b6dd2c69502daca05a3e2087f8d22
Bluesadsilk <bluesadsilk@proton.me>
2023-10-03 15:17:54 +0200

    Ejercicio de paridad acabado

--- EstructurasControl/Ejercicio3/paridad.java
+++ EstructurasControl/Ejercicio3/paridad.java
@@ -7,27 +7,27 @@ public class paridad {
     public static void main(String[] args) {
 
         int numIntr;
-        
-        Scanner teclado = new Scanner(System.in)
+
+        Scanner teclado = new Scanner(System.in);
 
         System.out.println("Introduzca un número para saber su paridad");
         numIntr = teclado.nextInt();
+        teclado.close();
 
         if (numIntr == 0) {
 
             System.out.println("El número no posee el valor paridad");
-            
+
         }
 
-        if (numIntr%2 = 0) {
+        if (numIntr % 2 == 0) {
 
             System.out.println("El número introducido es par");
+        }
 
-            else {
+        else {
 
-                System.out.println("El número introducido es inpar");
-            }
-            
+            System.out.println("El número introducido es inpar");
         }
 
     }
