68a00998061e6e8f9bfab4c6fa7c23e7206b8031
Bluesadsilk <bluesadsilk@proton.me>
2023-10-06 17:39:39 +0200

    cables

--- EstructurasControl3/Ejercicio1/compren.java
+++ EstructurasControl3/Ejercicio1/compren.java
@@ -1,5 +1,44 @@
 package EstructurasControl3.Ejercicio1;
 
+import java.util.Scanner;
+
 public class compren {
+
+    public static void main(String[] args) {
+
+        int n1;
+        int n2;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca dos números para conocer los números que comprenden el rango entre si.");
+        System.out.println("Introduzca el primer número");
+        n1 = teclado.nextInt();
+        System.out.println("Introduza el segundo número");
+        n2 = teclado.nextInt();
+        teclado.close();
+
+        int nmenor;
+        int nmayor;
+
+        if (n1 > n2) {
+
+            nmayor = n1;
+            nmenor = n2;
+
+        } else {
+
+            nmayor = n2;
+            nmenor = n1;
+        }
+
+        while (nmenor != nmayor + 1) {
+
+            System.out.println(nmenor);
+
+            nmenor++;
+
+        }
+
+    }
 
 }
