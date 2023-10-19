90c56b033c7c59c16d416d6dbc5464b79b2a9eb3
Bluesadsilk <bluesadsilk@proton.me>
2023-10-19 17:16:53 +0200

    Terminado el pdf 3 de estructuras de control.

--- EstructurasControl3/Ejercicio11/divisionFac.java
+++ EstructurasControl3/Ejercicio11/divisionFac.java
@@ -7,7 +7,57 @@ public class divisionFac {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduzca dos números para saber el resultado de su división");
+        System.out.println("Introduzca el dividendo");
+        int dividendo = teclado.nextInt();
+        System.out.println("Introduzca el divisor");
+        int divisor = teclado.nextInt();
+
+        while (true) {
+
+            if (dividendo >= 1) {
+
+                break;
+
+            }
+
+            else {
+
+                System.out.println("Introduzca un dividendo válido");
+                dividendo = teclado.nextInt();
+
+            }
+
+        }
+
+        while (true) {
+
+            if (divisor >= 1) {
+
+                break;
+            }
+
+            else {
+
+                System.out.println("Introduzca un divisor válido");
+                divisor = teclado.nextInt();
+
+            }
+
+        }
+
         teclado.close();
+
+        int resto = dividendo % divisor;
+        int contador = 0;
+
+        while (dividendo >= 1) {
+            dividendo = dividendo - divisor;
+            contador++;
+        }
+
+        System.out.println("El resultado es = " + contador + ", el resto es " + resto);
 
     }
 
