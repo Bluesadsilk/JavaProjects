40982d8cafd744280197b4023df97873ea2541dd
Bluesadsilk <bluesadsilk@proton.me>
2023-10-24 16:48:03 +0200

    ejercicio 4 dificil

--- EstructurasRepetitivas/Ejercicio2/primos.java
+++ EstructurasRepetitivas/Ejercicio2/primos.java
@@ -4,6 +4,29 @@ public class primos {
 
     public static void main(String[] args) {
 
+        boolean esprimo;
+        int num = 1, contprimo = 0;
+
+        while (contprimo < 20) {
+
+            esprimo = true;
+            for (int div = 2; div < num; div++) {
+                if (num % div == 0) {
+                    esprimo = false;
+                }
+
+            }
+
+            if (esprimo) {
+                System.out.println(num);
+                contprimo++;
+
+            }
+
+            num++;
+
+        }
+
     }
 
 }
--- EstructurasRepetitivas/Ejercicio4/secuencia.java
+++ EstructurasRepetitivas/Ejercicio4/secuencia.java
@@ -1,5 +1,29 @@
+
 package EstructurasRepetitivas.Ejercicio4;
 
-public class secuencia {
+import java.util.Scanner;
 
+public class secuencia {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce un número N: ");
+        int n = teclado.nextInt();
+        teclado.close();
+
+        // Imprime la secuencia ascendente
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        // Imprime las secuencias descendentes
+        for (int i = 2; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
