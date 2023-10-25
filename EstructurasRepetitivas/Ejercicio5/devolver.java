a41de29c1a5f89e3a4c5c018edd57d93ec3de4ad
Bluesadsilk <bluesadsilk@proton.me>
2023-10-25 17:42:45 +0200

    Ejercicio4 y Ejercicio5 cosas

--- EstructurasRepetitivas/Ejercicio5/devolver.java
+++ EstructurasRepetitivas/Ejercicio5/devolver.java
@@ -1,5 +1,30 @@
 package EstructurasRepetitivas.Ejercicio5;
 
+import java.util.Scanner;
+
 public class devolver {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca la cantidad de números que desea que se generen");
+        int cant = teclado.nextInt();
+        teclado.close();
+
+        for (int i = 1; i <= cant; i++) {
+
+            int imprimible = (int) ((Math.random() * Math.random()) * 10000);
+            while (imprimible > 0) {
+
+                if (imprimible % 10 == 0) {
+                    System.out.println(imprimible);
+                    break;
+                }
+                imprimible = imprimible / 10;
+
+            }
+
+        }
+
+    }
 
 }
--- EstructurasRepetitivas/Ejercicio6/eliminar.java
+++ EstructurasRepetitivas/Ejercicio6/eliminar.java
@@ -1,5 +1,28 @@
 package EstructurasRepetitivas.Ejercicio6;
 
+import java.util.Scanner;
+
 public class eliminar {
 
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca un número");
+        int num = teclado.nextInt();
+        teclado.close();
+
+        if (num <= 99) {
+
+            System.out.println("0");
+
+        }
+
+        else {
+
+            int num2 = num;
+
+            System.out.println(num2);
+
+        }
+
+    }
 }
