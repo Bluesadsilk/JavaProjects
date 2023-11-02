34bc9b0ad1c3ec0455c473ef434b668b772454bc
Bluesadsilk <bluesadsilk@proton.me>
2023-11-02 18:21:10 +0100

    haciendo mi primer menu

---
+++ Metodos_Intro/MenuMetodo/funcionesMenu.java
@@ -0,0 +1,21 @@
+package Metodos_Intro.MenuMetodo;
+
+public class funcionesMenu {
+
+    public void Cuadrado(int lado) {
+
+        for (int i = 0; i < lado; i++) {
+
+            for (int j = 0; j < lado; j++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println("");
+
+        }
+
+    }
+
+}
---
+++ Metodos_Intro/MenuMetodo/menuMetodo.java
@@ -0,0 +1,74 @@
+package Metodos_Intro.MenuMetodo;
+
+import java.util.Scanner;
+
+public class menuMetodo {
+
+    public static void main(String[] args) {
+        funcionesMenu menu = new funcionesMenu();
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca un núero para acceder al parámetro del menú");
+        System.out.println("1. Dibujar Cuadrado");
+        System.out.println("2. Calcular número digitos");
+        System.out.println("3. Recoger opción valida en un rango de valores");
+        System.out.println("4. Calcular el menor de 3 números");
+        System.out.println("5. Dibujar triángulo rectángulo de un número de filas");
+        System.out.println("6. Calcular el mayor de 3 números");
+        System.out.println("7. Dibujar trinagulo de un número de filas ");
+        System.out.println("8. Mostrar tabla de multiplicar");
+        System.out.println("9. Calcular media de 3 notas");
+        System.out.println("10. Calcular el sumatorio de un número");
+        System.out.println("11. Dibujar rombo");
+        System.out.println("12. Salir");
+
+        int option = teclado.nextInt();
+
+        switch (option) {
+
+            case 1:
+                System.out.println("Introduzca número");
+                menu.Cuadrado(teclado.nextInt());
+
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            case 8:
+                break;
+
+            case 9:
+                break;
+
+            case 10:
+                break;
+
+            case 11:
+                break;
+
+            case 12:
+                break;
+
+            default:
+                break;
+        }
+
+        teclado.close();
+    }
+
+}
