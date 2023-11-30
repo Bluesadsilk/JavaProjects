7275868f988fe040761ff76f0ca43e77b22a951e
Bluesadsilk <bluesadsilk@proton.me>
2023-11-30 19:07:15 +0100

    Ejercicio 3 de vectores

--- MatricesUnidimensionales/ej1/ej1.java
+++ MatricesUnidimensionales/ej1/ej1.java
@@ -14,11 +14,45 @@
 */
 package MatricesUnidimensionales.ej1;
 
+import java.util.Scanner;
+
 public class ej1 {
 
-    metodos_Ej1 menu = new metodos_Ej1();
+    public ej1() {
+    }
 
     public static void main(String[] args) {
+        metodos_Ej1 menu = new metodos_Ej1();
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Seleccione una de las opciones:");
+        System.out.println("1. Muestre la suma del rango y numero indicados.");
+        System.out.println("2. Muestra los arrays con los parámetros.");
+        System.out.println("3. Muestra la media de las notas y las notas por debajo de esta");
+        int num = teclado.nextInt();
+
+        switch (num) {
+            case 1:
+                menu.elUno();
+
+                break;
+            case 2:
+
+                menu.elDos();
+
+                break;
+
+            case 3:
+
+                menu.elTres();
+
+                break;
+
+            default:
+                break;
+        }
+
+        teclado.close();
 
     }
 
--- MatricesUnidimensionales/ej1/metodos_Ej1.java
+++ MatricesUnidimensionales/ej1/metodos_Ej1.java
@@ -41,9 +41,122 @@ public class metodos_Ej1 {
             }
         }
 
+        int res = 0;
+
         for (int i = 0; i < n; i++) {
 
-            System.out.println("Valor: " + i + " = " + vector[i]);
+            res += vector[i];
+
+            System.out.println(vector[i]);
+
+        }
+
+        System.out.println("Valor de rango: " + n + " = " + res);
+    }
+
+    public void elDos() {
+
+        int[] origen = { 10, 25, 26, 40, 37, 45, 90, 76, 78, 81, 58, 32 };
+        int contador = 0;
+
+        for (int i = 0; i < origen.length; i++) {
+
+            if (origen[i] % 2 == 0 && origen[i] > 25) {
+
+                contador++;
+
+            }
+
+        }
+
+        int[] destino = new int[contador];
+        int contador2 = 0;
+
+        for (int i = 0; i < origen.length; i++) {
+
+            if (origen[i] % 2 == 0 && origen[i] > 25) {
+
+                destino[contador2] = origen[i];
+                contador2++;
+
+            }
+
+        }
+
+        for (int i = 0; i < origen.length; i++) {
+            System.out.print(" " + origen[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < destino.length; i++) {
+            System.out.print(" " + destino[i] + " ");
+        }
+    }
+
+    public void elTres() {
+
+        int[] notas = new int[10];
+        int nA = 1;
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[0] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[1] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[2] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[3] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[4] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[5] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[6] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[7] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[8] = teclado.nextInt();
+
+        System.out.println("Introduzca la nota numero " + nA);
+        nA++;
+        notas[9] = teclado.nextInt();
+
+        int media = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+
+            media += notas[i];
+
+        }
+
+        media = media / notas.length;
+
+        System.out.print("La media es : " + media + " , las notas del suspenso son:");
+
+        for (int i = 0; i < notas.length; i++) {
+
+            if (notas[i] < media) {
+
+                System.out.print(" " + notas[i] + " ");
+            }
+
         }
 
     }
